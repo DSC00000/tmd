207ae7b6ede2276df9fe646391ed669a796f7030
Jeffrey G Berry <48kmem@gmail.com>
2022-11-07 10:46:12 -0600

    Add files via upload

---
+++ assets/js/auth.js
@@ -0,0 +1,43 @@
+import "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import "https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js";
+
+    const firebaseConfig = {						//Firebase App Data required for API
+        apiKey: "AIzaSyCYsOexRfQS1eCKXf4UXasY1N8HQ-ckWF0",
+        authDomain: "telemetry-upload.firebaseapp.com",
+        databaseURL: "https://telemetry-upload-default-rtdb.firebaseio.com",
+        projectId: "telemetry-upload",
+        storageBucket: "telemetry-upload.appspot.com",
+        messagingSenderId: "56055561946",
+        appId: "1:56055561946:web:9c0ace75c3352ac0df6fe3",
+        measurementId: "G-T3VQ6Q4R3M"
+    };
+
+    const app = initializeApp(firebaseConfig);
+
+    // 1) Create a new firebaseui.auth instance stored to our local variable ui
+    const ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+    // 2) These are our configurations.
+    const uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult(authResult, redirectUrl) {
+        return true
+        },
+        uiShown() {
+        document.getElementById("loader").style.display = "none"
+        },
+    },
+    signInFlow: "popup",
+    signInSuccessUrl: "signedIn",
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        // Additional login options should be listed here
+        // once they are enabled within the console.
+    ],
+    }
+
+    // 3) Call the 'start' method on our ui class
+    // including our configuration options.
+    ui.start("#firebaseui-auth-container", uiConfig)
---
+++ assets/js/rtdb.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getDatabase, ref, child, onValue } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
+            
+    const firebaseConfig = {						//Firebase App Data required for API
+        apiKey: "AIzaSyCYsOexRfQS1eCKXf4UXasY1N8HQ-ckWF0",
+        authDomain: "telemetry-upload.firebaseapp.com",
+        databaseURL: "https://telemetry-upload-default-rtdb.firebaseio.com",
+        projectId: "telemetry-upload",
+        storageBucket: "telemetry-upload.appspot.com",
+        messagingSenderId: "56055561946",
+        appId: "1:56055561946:web:9c0ace75c3352ac0df6fe3",
+        measurementId: "G-T3VQ6Q4R3M"
+    };
+    
+    const app = initializeApp(firebaseConfig);		//Standard database initialization
+    const database = getDatabase(app)
+    
+    getData.addEventListener('click',(e) => {		//Start the script on click for query button
+        $('#dataTable td').remove();
+
+        const dbRef = ref(database, `production/`);	//Look specifically at the production collection in the DB
+
+        onValue(dbRef, (snapshot) => {				//Go through each production child and assign it to a single variable
+            snapshot.forEach((childSnapshot) => {
+                const childData = childSnapshot.val();
+
+                var row = "<tr><td>"    + childData.Machine_Serial                        	+ "</td><td>"
+                                        + childData.Lifetime_Hours_On                     	+ "</td><td>"
+                                        + childData.Lifetime_Hours_Running_Product        	+ "</td><td>"
+                                        + childData.Lifetime_Louver_Count                 	+ "</td><td>"
+                                        + childData.Lifetime_Louvers_Drilled              	+ "</td><td>"
+                                        + childData.Lifetime_Louvers_Notched              	+ "</td><td>"
+                                        + childData.Lifetime_Louvers_Pinned               	+ "</td><td>"
+                                        + childData.Lifetime_Louvers_Stapled_to_Tilt_Rod  	+ "</td><td>"
+                                        + childData.Lifetime_Stile_Count 					+ "</td><td>"
+                                        + childData.Lifetime_Stile_Linear_Feet 				+ "</td></tr>";
+                
+                $(row).appendTo(dataTable)    		//Take the child variable and stick in the HTML table                        
+            });
+        }, {onlyOnce: true});						//Do the above one time
+    });
