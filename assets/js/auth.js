1202d2e5ac5121f0f430a2f27e1837883827da40
Jeffrey G Berry <48kmem@gmail.com>
2022-11-14 10:26:39 -0600

    Added auth gui to production.html

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -16,28 +16,41 @@ import "https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js";
     const app = initializeApp(firebaseConfig);
 
     // 1) Create a new firebaseui.auth instance stored to our local variable ui
-    const ui = new firebaseui.auth.AuthUI(firebase.auth())
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
     // 2) These are our configurations.
-    const uiConfig = {
-    callbacks: {
-        signInSuccessWithAuthResult(authResult, redirectUrl) {
-        return true
+    var uiConfig = {
+        callbacks: {
+          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+          },
+          uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+          }
         },
-        uiShown() {
-        document.getElementById("loader").style.display = "none"
-        },
-    },
-    signInFlow: "popup",
-    signInSuccessUrl: "signedIn",
-    signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // Additional login options should be listed here
-        // once they are enabled within the console.
-    ],
-    }
-
-    // 3) Call the 'start' method on our ui class
-    // including our configuration options.
-    ui.start("#firebaseui-auth-container", uiConfig)
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          //firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>',
+        // Privacy policy url.
+        privacyPolicyUrl: '<your-privacy-policy-url>'
+      };
+            
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
+      
