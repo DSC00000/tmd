81dd5902076d5391da517a1ad88e2279fb540c0a
Jeffrey G Berry <48kmem@gmail.com>
2022-11-15 15:30:55 -0600

    Sigh

--- assets/js/rtdb.js
+++ assets/js/rtdb.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
-import "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+import { GoogleAuthProvider, signInWithPopup, getAuth, initApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
 
 const firebaseConfig = {						//Firebase App Data required for API
     apiKey: "AIzaSyCYsOexRfQS1eCKXf4UXasY1N8HQ-ckWF0",
