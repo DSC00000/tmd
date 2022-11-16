b9a776766dc8dcb9a149f7db8d555f9aa8596753
Jeffrey G Berry <48kmem@gmail.com>
2022-11-16 16:29:39 -0600

    Minor housekeeping on statics

--- assets/js/statics.js
+++ assets/js/statics.js
@@ -55,12 +55,13 @@ document.getElementById("five").innerHTML = `
 `
 document.getElementById("footer").innerHTML = `
 <ul class="icons">
-<li><a href="https://www.facebook.com/texasmachinedesign" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
-<!-- <li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li> -->
-<li><a href="https://twitch.tv/texasmachinedesign" class="icon brands alt fa-twitch"><span class="label">Twitch</span></a></li>
-<li><a href="mailto:info@texasmachinedesign.com" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+    <li><a href="https://www.facebook.com/texasmachinedesign" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+    <!-- <li><a href="#" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li> -->
+    <li><a href="https://twitch.tv/texasmachinedesign" class="icon brands alt fa-twitch"><span class="label">Twitch</span></a></li>
+    <li><a href="mailto:info@texasmachinedesign.com" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
 </ul>
+
 <ul class="copyright">
-<li>&copy; TMD, LLC. All rights reserved. +1.512.772.4411</li><li>Design: <a href="http://html5up.net">HTML5 UP</a> and <a href="https://rubrnek.netlify.app">RUBRNEK</a></li>
+    <li>&copy; TMD, LLC. All rights reserved. +1.512.772.4411</li><li>Design: <a href="http://html5up.net">HTML5 UP</a> and <a href="https://rubrnek.netlify.app">RUBRNEK</a></li>
 </ul>
 `
